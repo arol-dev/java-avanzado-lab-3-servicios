8bd8ff2b767c77cc5d8c03ad3cbd98de6f305765
lcastellar <lcastellar@opentrends.com>
2025-09-17 19:44:49 +0200

    lcastellar: Terminado Lab: Servicios Eureka, Feign y Spring Cloud Gateway

--- orders-service/src/main/java/com/example/labs/orders/web/OrderController.java
+++ orders-service/src/main/java/com/example/labs/orders/web/OrderController.java
@@ -1,25 +1,67 @@
 package com.example.labs.orders.web;
 
+import com.example.labs.orders.clients.ProductsClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
 
+    ProductsClient productsClient;
+
+    public OrderController(ProductsClient productsClient) {
+        this.productsClient = productsClient;
+    }
+
+    @GetMapping("/orders-with-products")
+    public ResponseEntity<List<Map<String, Object>>> allWithProducts() {
+        List<Map<String, Object>> orders = getAllOrders();
+        List<Map<String, Object>> products = productsClient.all();
+
+        Map<Integer, Map<String, Object>> productsMap = products.stream()
+                .collect(Collectors.toMap(
+                        p -> (Integer) p.get("id"),
+                        p -> p
+                ));
+
+        List<Map<String, Object>> ordersWithProducts = orders.stream()
+                .map(order -> {
+                    Integer productId = (Integer) order.get("productId");
+                    Map<String, Object> product = productsMap.get(productId);
+
+                    Map<String, Object> orderWithProduct = new HashMap<>(order);
+                    if (product != null) {
+                        orderWithProduct.put("name", product.get("name"));
+                        orderWithProduct.put("price", product.get("price"));
+                    }
+                    return orderWithProduct;
+                }).collect(Collectors.toList());
+
+        return ResponseEntity.ok(
+                ordersWithProducts
+        );
+    }
+
     @GetMapping
     public ResponseEntity<List<Map<String, Object>>> all() {
         return ResponseEntity.ok(
-                List.of(
-                        Map.of("id", 100, "productId", 1, "date", LocalDate.now().toString()),
-                        Map.of("id", 101, "productId", 2, "date", LocalDate.now().minusDays(1).toString())
-                )
+                getAllOrders()
+        );
+    }
+
+    private List<Map<String, Object>> getAllOrders(){
+        return List.of(
+                Map.of("id", 100, "productId", 1, "date", LocalDate.now().toString()),
+                Map.of("id", 101, "productId", 2, "date", LocalDate.now().minusDays(1).toString())
         );
     }
 }
--- products-service/src/main/java/com/example/labs/products/web/ProductController.java
+++ products-service/src/main/java/com/example/labs/products/web/ProductController.java
@@ -1,10 +1,12 @@
 package com.example.labs.products.web;
 
+import com.example.labs.products.clients.OrdersClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,13 +14,38 @@ import java.util.Map;
 @RequestMapping("/api/products")
 public class ProductController {
 
+    OrdersClient ordersClient;
+
+    public ProductController(OrdersClient productsClient) {
+        this.ordersClient = productsClient;
+    }
+
+    @GetMapping("/products-with-orders")
+    public ResponseEntity<Map<String, List<Map<String, Object>>>> allWithProducts() {
+        List<Map<String, Object>> products = getAllProducts();
+        List<Map<String, Object>> orders = ordersClient.all();
+
+        // Devuelve un objeto con ambas listas o alguna correlación simple.
+        Map<String, List<Map<String, Object>>> resultMap = new HashMap<>();
+        resultMap.put("products", products);
+        resultMap.put("orders", orders);
+
+        return ResponseEntity.ok(
+                resultMap
+        );
+    }
+
     @GetMapping
     public ResponseEntity<List<Map<String, Object>>> all() {
         return ResponseEntity.ok(
-                List.of(
-                        Map.of("id", 1, "name", "Teclado", "price", 20.5),
-                        Map.of("id", 2, "name", "Mouse", "price", 10.0)
-                )
+                getAllProducts()
+        );
+    }
+
+    private List<Map<String, Object>> getAllProducts(){
+        return List.of(
+                Map.of("id", 1, "name", "Teclado", "price", 20.5),
+                Map.of("id", 2, "name", "Mouse", "price", 10.0)
         );
     }
 }
