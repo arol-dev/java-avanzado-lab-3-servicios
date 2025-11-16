b03ccef4da17f402d6d3d82b36c9697a2c57d5fc
davidd5 <dpb.9494@gmail.com>
2025-11-16 21:25:12 +0100

    exercise solved

--- orders-service/src/main/java/com/example/labs/orders/web/OrderController.java
+++ orders-service/src/main/java/com/example/labs/orders/web/OrderController.java
@@ -1,9 +1,12 @@
 package com.example.labs.orders.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.labs.orders.clients.ProductsClient;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,13 +16,13 @@ import java.util.Map;
 @RequestMapping("/api/orders")
 public class OrderController {
 
+    @Autowired
+    private ProductsClient productsClient;
+
     @GetMapping
     public ResponseEntity<List<Map<String, Object>>> all() {
-        return ResponseEntity.ok(
-                List.of(
-                        Map.of("id", 100, "productId", 1, "date", LocalDate.now().toString()),
-                        Map.of("id", 101, "productId", 2, "date", LocalDate.now().minusDays(1).toString())
-                )
-        );
+        List<Map<String,Object>> products = productsClient.getProducts(); // Llamada al servicio de Products
+        products.add(Map.of("name", "Altavoces", "price", 85.0, "id", 3));
+        return ResponseEntity.ok(products);
     }
 }
--- products-service/src/main/java/com/example/labs/products/clients/OrdersClient.java
+++ products-service/src/main/java/com/example/labs/products/clients/OrdersClient.java
@@ -1,16 +1,17 @@
 package com.example.labs.products.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * TODO (Estudiantes): Completar la configuración de Feign para llamar al servicio de Orders.
  * - Usar serviceId (name) o url. Para Eureka + Gateway se recomienda name="orders-service".
  * - Declarar el endpoint remoto: GET /api/orders
  */
-@FeignClient(name = "service-id")
+@FeignClient(name = "orders-service")
 public interface OrdersClient {
 
-    // TODO (Estudiantes): Ajustar el path si es necesario según el controlador remoto
-    // definir el método remoto para invocar al servicio de orders
+    @GetMapping("/api/orders")
+    public String getOrders();
 
 }
